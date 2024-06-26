baa14919a8e6469c0c611ad4594dc7b5d45e8e7c
Ekaterina_Lozhkina <lozhkina.ed@yandex.ru>
2024-06-26 20:37:51 +0400

    ДЗ Наследование и полиморфизм. П.2

--- src/TrainingGround.java
+++ src/TrainingGround.java
@@ -1,6 +1,13 @@
 public class TrainingGround {
     public static void main(String[] args){
         Hero hero = new Hero("Any hero");
+        Hero mage = new Mage("Nana");
+        Hero warrior = new Warrior("Ruby");
+        Hero archer = new Archer("Mia");
+
         hero.attackEnemy();
+        mage.attackEnemy();
+        warrior.attackEnemy();
+        archer.attackEnemy();
     }
 }
